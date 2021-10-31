b939df0589656d29bd85ea0a0f647ba177f9c480
tadam-m <63363267+tadam-m@users.noreply.github.com>
2021-10-31 12:28:09 +0100

    Add files via upload

---
+++ Flutter/examples/CoolLogin/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:techno_clubs_berlin/screens/auth/register_screen.dart';
+
+import 'package:techno_clubs_berlin/themes/themes.dart';
+import 'package:techno_clubs_berlin/screens/splash_screen.dart';
+import 'package:techno_clubs_berlin/screens/auth/login_screen.dart';
+
+import 'constants/routes.dart';
+
+void main() {
+  runApp(const Routing());
+}
+
+class Routing extends StatelessWidget {
+  const Routing({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      theme: Themes().darkTheme,
+      initialRoute: '/',
+      getPages: <GetPage<dynamic>>[
+        GetPage<SplashScreen>(
+          name: '/',
+          page: () => const SplashScreen(),
+        ),
+        GetPage<LoginScreen>(
+          name: loginRoute,
+          page: () => const LoginScreen(),
+        ),
+        GetPage<RegisterScreen>(
+          name: registerRoute,
+          page: () => const RegisterScreen(),
+        ),
+        // GetPage<LoginScreen>(
+        //   name: loginRoute,
+        //   page: () => LoginScreen(),
+        // ),
+        // GetPage<RegisterScreen>(
+        //   name: registerRoute,
+        //   page: () => RegisterScreen(),
+        // ),
+      ],
+    );
+  }
+}
+
