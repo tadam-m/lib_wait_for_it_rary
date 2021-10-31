4d141a21173e143a85a5711320c586ba095e1ab4
tadam-m <63363267+tadam-m@users.noreply.github.com>
2021-10-31 12:17:34 +0100

    Update catchServerError.dart

--- Flutter/ErrorHandling/catchServerError.dart
+++ Flutter/ErrorHandling/catchServerError.dart
@@ -1,1 +1,23 @@
+// USAGE var responseJson = catchServerError(response);
 
+//   if (response.statusCode == 200) -> return content;
+//   else
+//     throw Exception(responseJson.toString());
+
+// Custom your execptions on custom_exeptions.dart
+
+static dynamic catchServerError(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        var responseJson = json.decode(response.body.toString());
+        return responseJson;
+      case 401:
+        throw BadRequestException(response.body.toString());
+      case 403:
+        throw UnauthorisedException(response.body.toString());
+      case 500:
+      default:
+        throw FetchDataException(
+            'Error occured while Communication with Server with StatusCode: ${response.statusCode}');
+    }
+}
